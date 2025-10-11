70a6771dcfe880800aad41e5e5997f4b124d9d1c
Johannes Raggam <thetetet@gmail.com>
2025-10-11 04:09:55 +0200

    Fix rendering error with main template.

--- src/thet/spa/browser/main_template.py
+++ src/thet/spa/browser/main_template.py
@@ -1,12 +1,13 @@
 from Products.CMFPlone.browser import main_template
 from Products.CMFPlone.browser.interfaces import IMainTemplate
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.interface import implementer
 
 
 @implementer(IMainTemplate)
 class MainTemplate(main_template.MainTemplate):
-    ajax_template_name = "templates/main_template_ajax.pt"
-    main_template_name = "templates/main_template.pt"
+    ajax_template_name = ViewPageTemplateFile("main_template_ajax.pt")
+    main_template_name = ViewPageTemplateFile("main_template.pt")
 
 
 class MainTemplateContent(MainTemplate):
