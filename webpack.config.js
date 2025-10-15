b2b42022c6c8ff99b44698062f591b27b722dd0e
Johannes Raggam <thetetet@gmail.com>
2025-10-15 06:01:18 +0200

    Webpack config: Compile Mockup#thet-improvements and the bundle from this repo.

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,16 @@ const path = require("path");
 const package_json = require("./package.json");
 const package_json_mockup = require("@plone/mockup/package.json");
 const package_json_patternslib = require("@patternslib/patternslib/package.json");
-const webpack_config = require("@patternslib/dev/webpack/webpack.config").config;
+const webpack_config = require("@plone/mockup/webpack.config");
 const mf_config = require("@patternslib/dev/webpack/webpack.mf");
 
 module.exports = () => {
-    let config = {
-        entry: {
-            "bundle.min": path.resolve(__dirname, "resources/index"),
-        },
-    };
+    const config = webpack_config();
 
-    config = webpack_config({
-        config: config,
-        package_json: package_json,
-    });
+    config.entry["collective-ajaxify.bundle.min"] = path.resolve(
+        __dirname,
+        "resources/index"
+    );
     config.output.path = path.resolve(
         __dirname,
         "src/collective/ajaxify/browser/static/scripts/"
@@ -24,8 +20,8 @@ module.exports = () => {
     config.plugins.push(
         mf_config({
             name: package_json.name,
-            filename: "remote.min.js",
-            remote_entry: config.entry["bundle.min"],
+            filename: "collective-ajaxify.remote.min.js",
+            remote_entry: config.entry["collective-ajaxify.bundle.min"],
             dependencies: {
                 ...package_json_patternslib.dependencies,
                 ...package_json_mockup.dependencies,
@@ -33,12 +29,6 @@ module.exports = () => {
             },
         })
     );
-
-    if (process.env.NODE_ENV === "development") {
-        config.devServer.port = "8011";
-        config.devServer.static.directory = path.resolve(__dirname, "resources/");
-        config.devServer.liveReload = false;
-    }
 
     return config;
 };
