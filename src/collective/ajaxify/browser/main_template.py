74672f7cde776f4b34860b87bce9ce3415f43a30
Peter Mathis <peter.mathis@kombinat.at>
2025-11-24 16:56:42 +0100

    re-implement main_template logic for Plone 6.1

--- src/collective/ajaxify/browser/main_template.py
+++ src/collective/ajaxify/browser/main_template.py
@@ -2,12 +2,44 @@ from Products.CMFPlone.browser import main_template
 from Products.CMFPlone.browser.interfaces import IMainTemplate
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.interface import implementer
+from plone.memoize.view import memoize
 
 
 @implementer(IMainTemplate)
 class MainTemplate(main_template.MainTemplate):
+    """ reimplementation of 6.2 main_template logic for Plone 6.1 """
+
     ajax_template = ViewPageTemplateFile("main_template_ajax.pt")
     main_template = ViewPageTemplateFile("main_template.pt")
+
+    def __call__(self):
+        return self.template()
+
+    @property
+    @memoize
+    def use_ajax(self):
+        try:
+            # Plone 6.2 has "use_ajax" factory.
+            return super().use_ajax
+        except AttributeError:
+            pass
+
+        if "ajax_load" in self.request:
+            return bool(self.request.get("ajax_load", False))
+
+        # defaults to False
+        return False
+
+    @property
+    def template(self):
+        if self.use_ajax:
+            return self.ajax_template
+        else:
+            return self.main_template
+
+    @property
+    def macros(self):
+        return self.template.macros
 
 
 class MainTemplateContent(MainTemplate):
@@ -16,5 +48,5 @@ class MainTemplateContent(MainTemplate):
     """
 
     @property
-    def template_name(self):
-        return "templates/main_template_ajax.pt"
+    def template(self):
+        return self.ajax_template
